3ffbf547b99c410fb207885d3d673ccec3195835
Krushnal Patel <krushnalpatel11@gmail.com>
2021-05-19 18:55:53 +0530

    added responsiveness

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,10 +1,27 @@
 var current_element = document.getElementById('profile');
+var menuStatus;
 
 function show_section(section) {
-    console.log(current_element);
     var element = document.getElementById(section);
-    console.log(element, '\n\n\n', current_element);
     current_element.style.display = "none";
     element.style.display = "revert";
     current_element = element;
+    const windowWidth = window.innerWidth;
+    if (windowWidth > 1000)
+        menuStatus = true;
+    else menuStatus = false;
+    toggleMenu();
+}
+
+function toggleMenu() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth < 1000)
+    if (!menuStatus) {
+        document.getElementById("mySidenav").style.display = "none";
+        menuStatus = true;
+    }
+    else {
+        document.getElementById("mySidenav").style.display = "flex";
+        menuStatus = false;
+    }
 }
